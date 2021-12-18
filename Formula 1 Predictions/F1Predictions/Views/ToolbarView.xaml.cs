394501d02ff878b61b5e1e270c2e1218f8dabcba
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2021-12-18 12:30:22 +0000

    Toolbar View renders - still some issues to figure out

--- Formula 1 Predictions/F1Predictions/Views/ToolbarView.xaml.cs
+++ Formula 1 Predictions/F1Predictions/Views/ToolbarView.xaml.cs
@@ -1,7 +1,12 @@
+using F1Predictions.Core.ViewModels;
+using MvvmCross.Platforms.Wpf.Presenters.Attributes;
 using MvvmCross.Platforms.Wpf.Views;
+using MvvmCross.ViewModels;
 
 namespace F1Predictions.Views;
 
+[MvxContentPresentation]
+[MvxViewFor(typeof(ToolbarViewModel))]
 public partial class ToolbarView : MvxWpfView
 {
     public ToolbarView()
