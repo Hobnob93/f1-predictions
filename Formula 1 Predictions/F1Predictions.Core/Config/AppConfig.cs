8b1b35a8e2e2e56c8b88725658ca318007a6a540
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2021-12-21 20:56:59 +0000

    Created the Application and Toolbar config classes

---
+++ Formula 1 Predictions/F1Predictions.Core/Config/AppConfig.cs
@@ -0,0 +1,10 @@
+namespace F1Predictions.Core.Config;
+
+public class AppConfig
+{
+    public const string Section = "Application";
+    
+    public string Name { get; set; }
+    public string BackgroundColor { get; set; }
+    public string DefaultFont { get; set; }
+}
--- Formula 1 Predictions/F1Predictions/Startup.cs
+++ Formula 1 Predictions/F1Predictions/Startup.cs
@@ -1,6 +1,8 @@
+using System.IO;
 using System.Windows;
 using F1Predictions.Core.Interfaces;
 using F1Predictions.Core.Services;
+using Microsoft.Extensions.Configuration;
 using Prism.Ioc;
 using Serilog;
 using Serilog.Extensions.Logging;
@@ -9,17 +11,24 @@ namespace F1Predictions;
 
 public class Startup
 {
-    public Startup()
+    private readonly IConfiguration _config;
+    
+    public Startup() : this(AddConfiguration())
     {
-        
     }
 
+    public Startup(IConfiguration config)
+    {
+        _config = config;
+    }
+    
+    
     public void Initialize(StartupEventArgs e)
     {
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Debug()
             .WriteTo.Console()
-            .WriteTo.File("logs\\_Log.txt", rollingInterval: RollingInterval.Hour)
+            .WriteTo.File("logs\\_Log.txt", rollingInterval: RollingInterval.Day)
             .CreateLogger();
     }
     
@@ -35,5 +44,13 @@ public class Startup
     {
         var appLogger = new SerilogLoggerProvider(Log.Logger).CreateLogger("App");
         containerRegistry.RegisterInstance(appLogger);
+    }
+
+    private static IConfiguration AddConfiguration()
+    {
+        return new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
     }
 }
