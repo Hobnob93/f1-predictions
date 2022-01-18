161e280856a6936443168a578feafbb8f588c0d3
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-01-18 21:04:03 +0000

    £Complete PredictionStore

--- Formula 1 Predictions/F1Predictions.Core/Dtos/AnswerFetchDto.cs
+++ Formula 1 Predictions/F1Predictions.Core/Dtos/AnswerFetchDto.cs
@@ -2,6 +2,7 @@ namespace F1Predictions.Core.Dtos;
 
 public record AnswerFetchDto
 {
+    public string Section { get; set; }
     public string Question { get; set; }
     public string[] Answers { get; set; }
     public string[] Notes { get; set; }
--- Formula 1 Predictions/F1Predictions.Core/Services/AnswerStore.cs
+++ Formula 1 Predictions/F1Predictions.Core/Services/AnswerStore.cs
@@ -1,3 +1,4 @@
+using F1Predictions.Core.Config;
 using F1Predictions.Core.Dtos;
 using F1Predictions.Core.Interfaces;
 
@@ -6,15 +7,57 @@ namespace F1Predictions.Core.Services;
 public class AnswerStore : IAnswerStore
 {
     private readonly IGoogleSheets sheets;
-
-    public AnswerStore(IGoogleSheets sheets)
+    private readonly PredictionConfig config;
+    
+    private IList<IList<AnswerFetchDto>> answers;
+    
+    public AnswerStore(IGoogleSheets sheets, PredictionConfig config)
     {
         this.sheets = sheets;
+        this.config = config;
+        
+        InitializePredictions();
+    }
+    
+    private void InitializePredictions()
+    {
+        var sections = sheets.FetchAllPredictions().ToList();
+
+        answers = new List<IList<AnswerFetchDto>>();
+
+        foreach (var sectionConfig in config.PredictionSections)
+        {
+            var answersForSection = new List<AnswerFetchDto>();
+
+            for (var q = 0; q < sectionConfig.QuestionCount; q++)
+            {
+                var row = sectionConfig.StartingRow + 2 + q;
+                var sectionData = sections[row].ToArray();
+                var sectionNameData = sections[sectionConfig.StartingRow];
+                
+                var answersEnd = config.MaxAnswersPerQuestion + 1;
+
+                answersForSection.Add(new AnswerFetchDto
+                {
+                    Section = sectionNameData?.FirstOrDefault()?.ToString() ?? "Title Not Found",
+                    Question = sectionData.FirstOrDefault()?.ToString() ?? "Question Not Found",
+                    Answers = sectionData.Take(1..answersEnd)
+                        .Select(o => o.ToString())
+                        .ToArray(),
+                    Notes = sectionData.Take(answersEnd..^1)
+                        .Select(o => o.ToString())
+                        .ToArray(),
+                    Scoring = sectionData.LastOrDefault()?.ToString() ?? "No scoring could be found"
+                });
+            }
+            
+            answers.Add(answersForSection);
+        }
     }
     
     
     public AnswerFetchDto FetchAnswer(int sectionIndex, int questionIndex)
     {
-        throw new NotImplementedException();
+        return answers[sectionIndex][questionIndex];
     }
 }
