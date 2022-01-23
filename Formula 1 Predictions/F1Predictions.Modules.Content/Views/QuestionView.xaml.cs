bdf059dadfa34c48e99aaa013f567070b305cf4c
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-01-23 11:10:43 +0000

    Details section of new Question View

---
+++ Formula 1 Predictions/F1Predictions.Modules.Content/Views/QuestionView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace F1Predictions.Modules.Content.Views;
+
+public partial class QuestionView : UserControl
+{
+    public QuestionView()
+    {
+        InitializeComponent();
+    }
+}
