526943471162ba534def4dc4aa745cad2b0161f7
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-01-29 15:07:15 +0000

    Ability to hide participant data

--- Formula 1 Predictions/F1Predictions.Core/Components/ParticipantBanner.xaml.cs
+++ Formula 1 Predictions/F1Predictions.Core/Components/ParticipantBanner.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace F1Predictions.Core.Components;
 
@@ -18,6 +19,18 @@ public partial class ParticipantBanner : UserControl
     
     public static readonly DependencyProperty ColorProperty = 
         DependencyProperty.Register(nameof(Color), typeof(string), typeof(ParticipantBanner), new PropertyMetadata(string.Empty));
+    
+    public static readonly DependencyProperty HideProperty = 
+        DependencyProperty.Register(nameof(Hide), typeof(bool), typeof(CompetitorBanner), new PropertyMetadata(false));
+    
+    public static readonly DependencyProperty VTextProperty = 
+        DependencyProperty.Register(nameof(VText), typeof(string), typeof(CompetitorBanner), new PropertyMetadata(string.Empty));
+    
+    public static readonly DependencyProperty VDataProperty = 
+        DependencyProperty.Register(nameof(VData), typeof(string), typeof(CompetitorBanner), new PropertyMetadata(string.Empty));
+    
+    public static readonly DependencyProperty VColorProperty = 
+        DependencyProperty.Register(nameof(VColor), typeof(string), typeof(CompetitorBanner), new PropertyMetadata(string.Empty));
 
     public string Text
     {
@@ -35,5 +48,49 @@ public partial class ParticipantBanner : UserControl
     {
         get => (string) GetValue(ColorProperty);
         set => SetValue(ColorProperty, value);
+    }
+    
+    public bool Hide
+    {
+        get => (bool) GetValue(HideProperty);
+        set => SetValue(HideProperty, value);
+    }
+    
+    public string VText
+    {
+        get => (string) GetValue(VTextProperty);
+        set => SetValue(VTextProperty, value);
+    }
+    
+    public string VData
+    {
+        get => (string) GetValue(VDataProperty);
+        set => SetValue(VDataProperty, value);
+    }
+    
+    public string VColor
+    {
+        get => (string) GetValue(VColorProperty);
+        set => SetValue(VColorProperty, value);
+    }
+
+    private void ParticipantBanner_OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton != MouseButton.Left || !Hide)
+            return;
+        
+        VText = Text;
+        VColor = Color;
+        VData = Data;
+    }
+
+    private void ParticipantBanner_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (Hide)
+            return;
+        
+        VText = Text;
+        VColor = Color;
+        VData = Data;
     }
 }
